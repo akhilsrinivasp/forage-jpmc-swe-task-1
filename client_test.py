64e89f0c33f23899ee3ffd5963c95dd6158d9cf9
Akhil Srinivas P <akhilsrinivasp@gmail.com>
2023-06-13 19:51:24 +0530

    Added Tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,18 +8,43 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2))
+      
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
-
-
+  def test_getRatio_calculateRatio(self):
+    """Test if the function returns the correct ratio
+    """
+    price_a = 120.48
+    price_b = 117.87
+    self.assertEqual(getRatio(price_a, price_b), price_a/price_b)
+  
+  def test_getDataPoint_emptyQuotes(self):
+    """Test if the function throws an error when the quotes list is empty
+    """
+    quotes = []
+    for quote in quotes:
+      self.assertRaises(getDataPoint(quote))
+      
+  def test_getDataPoint_missingKeys(self):
+    """Test if the function throws an error when the quotes list is missing keys
+    """
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771'}
+    ]
+    for quote in quotes:
+      self.assertRaises(getDataPoint(quote))
+  
 
 if __name__ == '__main__':
     unittest.main()
